c96a57a72004e82571b77ec24602529d05647e3f
Artem Tabachenko <fexo5467@gmail.com>
2024-12-16 17:37:50 +0300

    Initial Commit

---
+++ VKRCoverageKit/VKRCoverageKit.h
@@ -0,0 +1,18 @@
+//
+//  VKRCoverageKit.h
+//  VKRCoverageKit
+//
+//  Created by Artyom Tabachenko on 16.12.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VKRCoverageKit.
+FOUNDATION_EXPORT double VKRCoverageKitVersionNumber;
+
+//! Project version string for VKRCoverageKit.
+FOUNDATION_EXPORT const unsigned char VKRCoverageKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VKRCoverageKit/PublicHeader.h>
+
+
